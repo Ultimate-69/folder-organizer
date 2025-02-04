ae0fe703037c54aae0ce82ce6e9ad20e19323dd4
Ultimate-69 <170136337+Ultimate-69@users.noreply.github.com.>
2025-02-04 14:38:16 +0200

    added basic checking to get path

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 import os
 
 def main():
-    print("Test")
+    isValidPath = False
+    path = ""
+
+    while not isValidPath:
+        try:
+            path = input("Please enter the path to the directory: ")
+        except ValueError:
+            print("Error! Please try again.")
+            continue
+
+        isValidPath = os.path.exists(path)
+        if not isValidPath:
+            print("Not a valid path!")
+        elif not os.path.isdir(path):
+            print("Please provide a folder, not a file!")
+            isValidPath = False
+
+
+
 
 if __name__ == "__main__":
     main()
